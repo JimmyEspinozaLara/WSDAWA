a17bb5f59ce8318954e73567714542dd7cceb647
JimmyEspinozaLara <jimmy.espinozalar@ug.edu.ec>
2024-07-28 20:50:08 -0500

    tokend_publicaciones

--- ws_dawa/src/api/Services/posts_service.py
+++ ws_dawa/src/api/Services/posts_service.py
@@ -1,15 +1,22 @@
 from flask_restful import Resource
+from flask import request
 
 from ..Components.posts_component import PublicacionesComponent
 from ...utils.general.logs import HandleLogs
 from ...utils.general.response import response_error, response_success
-
+from ..Components.jwt_component import JwtComponent
 
 class PublicacionListService(Resource):
     @staticmethod
     def get():
         try:
             HandleLogs.write_log("Ejecutando servicio para obtener todas las publicaciones")
+
+            # Validar el Token de seguridad
+            token = request.headers.get('tokenapp')
+            if not token or not JwtComponent.TokenValidate(token):
+                return response_error("Token de autenticación NO válido")
+
             resultado = PublicacionesComponent.getAllPublicaciones()
             if resultado['result']:
                 return response_success(resultado['data'])
