fba3d80dcda4915fddc8815ea3d8317bcb4061a9
David Miranda <davidjarod1111@gmail.com>
2024-07-29 19:51:19 -0500

    Update 19:51

--- ws_dawa/frontend/src/App.js
+++ ws_dawa/frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
+import Profile from './components/Profile'; // Asegúrate de que el componente Profile esté en esta ruta
 
 function App() {
   return (
@@ -11,12 +12,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
         {/* Ruta por defecto, redirige a /login */}
         <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
-
