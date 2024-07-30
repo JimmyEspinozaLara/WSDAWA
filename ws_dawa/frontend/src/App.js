0711647bfa5ee014d0139516f96d92c947851d65
David Miranda <davidjarod1111@gmail.com>
2024-07-29 19:14:54 -0500

    Update App.js

--- ws_dawa/frontend/src/App.js
+++ ws_dawa/frontend/src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
-import Posts from './components/Posts';
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/posts" element={<Posts />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        {/* Ruta por defecto, redirige a /login */}
+        <Route path="/" element={<Login />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+
