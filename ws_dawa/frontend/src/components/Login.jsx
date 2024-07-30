49a99713892a956c5d60a5a5dc455c53de552835
JimmyEspinozaLara <jimmy.espinozalar@ug.edu.ec>
2024-07-30 01:11:31 -0500

    login

--- ws_dawa/frontend/src/components/Login.jsx
+++ ws_dawa/frontend/src/components/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
 
   const manejadorLogin = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:3200/security/login", {
+      const response = await fetch("http://localhost:1008/security/login", {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
